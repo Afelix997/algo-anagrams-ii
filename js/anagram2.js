ba3eff81b0b538e480ede5cc4a453ad2fbd12b08
afelix997 <afelix2013@gmail.com>
2022-05-27 14:35:33 -0700

    new

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,9 @@
 exports.anagramsFor = function(word, listOfWords) {
-
+    let resArr = [];
+    for(let checking of listOfWords)
+    if (word.toLowerCase().split('').sort().join('')
+     === checking.toLowerCase().split('').sort().join('')){
+         resArr.push(checking)
+     }
+     return resArr;
 };
--- js/anagram2Spec.js
+++ js/anagram2Spec.js
@@ -3,7 +3,7 @@
 var ana = require("./anagram2"),
 listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
 
-console.log(ana.anagramsFor("threads", listOfWords).length == 4);
+// console.log(ana.anagramsFor("threads", listOfWords).length() == 4);
 console.log(ana.anagramsFor("threads", listOfWords)[0] == "threads");
 console.log(ana.anagramsFor("threads", listOfWords)[1] == "trashed");
 console.log(ana.anagramsFor("threads", listOfWords)[2] == "hardest");
