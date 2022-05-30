2eff45c267df433f4f1e2ccf5498ea29e3c9da26
afelix997 <afelix2013@gmail.com>
2022-05-29 17:08:44 -0700

    answer!

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,15 @@
-# Don't forget to run the tests (and create some of your own)
+from hashlib import new
+
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	res_list=[]
+	new_word= list(word.lower())
+	new_word.sort()
+	for checking in list_of_words:
+		new_checking= list(checking.lower())
+		new_checking.sort()
+		if  "".join(new_word)== "".join(new_checking):
+			res_list.append(checking)
+	return res_list
+
+
